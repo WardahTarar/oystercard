171a0c23022b1ec702637caaab14474472411aa0
Wardah <wlt_93@hotmail.com>
2019-03-11 15:14:02 +0000

    Passes test for top_up method existence with one argument

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,4 +6,10 @@ it 'freshly initialized card has a balance of 0' do
 expect(subject.balance).to eq 0
 end
 
+it {is_expected.to respond_to(:top_up).with(1).argument }
+
+# it "checks the top up amount added to balance" do
+#   expect(subject.top_up(100)).to eq 100
+# end
+
 end
