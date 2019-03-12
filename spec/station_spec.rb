81d3e71d8c7bcc9d0c7c79c47d55ec6adff36dfe
Wardah <wlt_93@hotmail.com>
2019-03-12 22:34:04 +0000

    Corrected errors

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,12 +1,11 @@
 require 'station'
 
 describe Station do
-it "checks for station name" do
-  expect(subject.name).to eq nil
-end
+  it "checks for station name" do
+    expect(subject.name).to eq nil
+  end
 
-it "checks for station zone" do
-  expect(subject.zone).to eq nil
-end
-
+  it "checks for station zone" do
+    expect(subject.zone).to eq nil  
+  end  
 end
